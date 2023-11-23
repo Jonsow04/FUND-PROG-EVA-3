2c6cf2866068b7e547f1847e85e4d57926d44e44
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2023-11-22 19:13:11 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,15 @@
+
+package eva3_1_funciones;
+
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+        //LLAMADA FUNCIÓN
+        impresarMensajado("Hoooola.");
+    }
+    
+    public static void impresarMensajado(String mensage){
+        System.out.println(mensage);
+    }
+    
+}
